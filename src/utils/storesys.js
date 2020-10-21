285a4f636615910c6d60e19b06741c47ae79a04d
taoxhsmile <taoxhsmile@gmail.com>
2020-10-21 16:13:41 +0800

    修改方法名称

--- src/utils/storesys.js
+++ src/utils/storesys.js
@@ -1,7 +1,9 @@
-export const storesysId = (function fn() {
+const getStoresysIdFromUrl = () => {
   let res = location.host.match(/^shop_([^.]+)\.m\.xzintl\.com$/);
   if (res && res[1]) {
     return res[1].toUpperCase();
   }
   return "";
-})();
+};
+
+export const storesysId = getStoresysIdFromUrl();
