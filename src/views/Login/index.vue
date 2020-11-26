cfd97400a911c010169a23fa7f1696127e974a5f
陶新华 <taoxinhua@taoxinhuadeMacBook-Pro.local>
2020-11-26 15:09:16 +0800

    修改登录页面样式

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,45 +1,17 @@
 <template>
   <div class="page-login">
-    <template v-if="needWechatLogin">
-      登录中...
-    </template>
-    <template v-else>
-      <van-field v-model="mobile" colon placeholder="请输入" label="手机号" />
-      <van-field
-        v-model="code"
-        colon
-        placeholder="请输入"
-        maxlength="6"
-        label="验证码"
-      />
-      <!-- <van-field v-model="userId" colon placeholder="请输入" label="userId" />
-      <van-field
-        v-model="storesysId"
-        colon
-        placeholder="请输入"
-        label="storesysId"
-      />
-      <van-field
-        v-model="accessToken"
-        colon
-        placeholder="请输入"
-        label="accessToken"
-      />
-      <van-field
-        v-model="tokenTime"
-        colon
-        placeholder="请输入"
-        label="tokenTime"
-      />
-      <van-field
-        v-if="openidStr"
-        v-model="openidStr"
-        colon
-        placeholder="请输入"
-        label="openidStr"
-      /> -->
-      <van-button type="primary" block @click="login">登录</van-button>
-    </template>
+    <h1>手机号快捷登录</h1>
+    <div class="form">
+      <div class="form-item">
+        <input type="tel" maxlength="11" placeholder="请输入手机号" />
+      </div>
+      <div class="form-item">
+        <input type="tel" maxlength="4" placeholder="请输入验证码" />
+        <!-- 重新发送(58s） -->
+        <button class="getcode-btn" disabled>发送验证码</button>
+      </div>
+      <button class="login-btn" disabled>确定</button>
+    </div>
   </div>
 </template>
 <script>
@@ -78,39 +50,6 @@ export default {
     }
   },
   methods: {
-    // login() {
-    //   let {
-    //     userId,
-    //     storeId,
-    //     storesysId,
-    //     accessToken,
-    //     tokenTime,
-    //     redirectUrl
-    //   } = this;
-
-    //   CookieSet("userId", userId);
-    //   CookieSet("userInfo", {
-    //     userInnerId: 2,
-    //     userUnionId: "oJjHK6mVxKdg6iC0z1ZiPeYF4hXE",
-    //     userAvatar: "wechatimage/2020/09/08/j3odql6w54_wximage.png",
-    //     userPhone: "15757185531",
-    //     userNickname: "smile_微笑",
-    //     userGender: 1,
-    //     userName: "陶新华",
-    //     userBirthday: 780336000000
-    //   });
-    //   CookieSet("accessToken", accessToken);
-    //   CookieSet("tokenTime", tokenTime);
-
-    //   Toast("登录成功");
-    //   setTimeout(() => {
-    //     if (redirectUrl) {
-    //       this.$replace(redirectUrl);
-    //     } else {
-    //       this.$replace("/");
-    //     }
-    //   }, 800);
-    // }
     login() {
       let { mobile, code, openidStr, redirectUrl, bindPhone } = this;
       let postUrl, postData;
@@ -156,5 +95,93 @@ export default {
 </script>
 <style lang="less" scoped>
 .page-login {
+  height: 100vh;
+  background: white;
+  h1 {
+    width: 310px;
+    margin: 0 auto;
+    padding: 42px 0;
+    font-size: 24px;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: #000000;
+    line-height: 34px;
+  }
+  .form {
+    .form-item {
+      margin: 0 auto;
+      margin-top: 15px;
+      width: 310px;
+      height: 42px;
+      background: #fff5ee;
+      border-radius: 5px;
+      position: relative;
+      &:first-child {
+        margin-top: 0;
+      }
+      input {
+        display: block;
+        width: 100%;
+        height: 42px;
+        line-height: 42px;
+        background: none;
+        border: none;
+        font-size: 14px;
+        font-family: PingFangSC-Regular, PingFang SC;
+        font-weight: 400;
+        box-sizing: border-box;
+        padding: 0 14px;
+        outline: none;
+        &::-webkit-input-placeholder {
+          color: #bfb8b8;
+        }
+      }
+      .getcode-btn {
+        position: absolute;
+        width: 106px;
+        top: 50%;
+        transform: translateY(-50%);
+        right: 0;
+        border: none;
+        outline: none;
+        background: none;
+        font-size: 14px;
+        font-family: PingFangSC-Regular, PingFang SC;
+        font-weight: 400;
+        color: #ff6a00;
+        line-height: 20px;
+        &:before {
+          content: "";
+          position: absolute;
+          left: 0;
+          top: 50%;
+          transform: translateY(-50%);
+          border-left: 1px solid #ff6a00;
+          height: 15px;
+        }
+        &[disabled] {
+          color: #909399;
+        }
+      }
+    }
+    .login-btn {
+      display: block;
+      margin: 0 auto;
+      margin-top: 42px;
+      width: 310px;
+      height: 44px;
+      background: #ff6a00;
+      border-radius: 24px;
+      border: none;
+      color: white;
+      font-size: 18px;
+      font-family: PingFangSC-Regular, PingFang SC;
+      font-weight: 400;
+      outline: none;
+      &[disabled] {
+        background: #e5e5e5;
+      }
+    }
+  }
 }
 </style>
