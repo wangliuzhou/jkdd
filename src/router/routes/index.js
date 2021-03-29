349988d06dc58ebfd9a2e5d3ee7d8b53c5794fea
wangliuzhou <961446601@qq.com>
2021-03-30 00:04:29 +0800

    s

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -5,6 +5,11 @@ const routes = [
     component: () => import("@/views/Index.vue")
   },
   {
+    path: "/test",
+    name: "Test",
+    component: () => import("@/views/Test.vue")
+  },
+  {
     path: "/login",
     name: "Login",
     meta: {},
